e1e7a7d36ee10635faea604ef1b55220f7286f1e
JohnnySeubert <jbseubert@gmail.com>
2025-09-21 16:33:35 -0300

    Refactor tests for address type detection to improve parallel execution and structure

--- testes_automatizados/enderecos/endereco_test.go
+++ testes_automatizados/enderecos/endereco_test.go
@@ -8,6 +8,7 @@ type cenarioDeTeste struct {
 }
 
 func TestTipoDeEndereco(test *testing.T) {
+	test.Parallel() // Diz que os testes que tem essa flag pode ser feito em paralelo caso queira
 
 	cenariosDeTeste := []cenarioDeTeste{
 		{enderecoInserido: "Rua Almirante Saldanha da Gama", retornoEsperado: "RUA"},
@@ -28,7 +29,30 @@ func TestTipoDeEndereco(test *testing.T) {
 				cenario.enderecoInserido,
 			)
 		}
+	}
+}
 
+func TestTipoDeEndereco2(test *testing.T) {
+	test.Parallel() // Diz que os testes que tem essa flag pode ser feito em paralelo caso queira
+
+	cenariosDeTeste := []cenarioDeTeste{
+		{enderecoInserido: "Rua Almirante Saldanha da Gama", retornoEsperado: "RUA"},
+		{enderecoInserido: "Avenida Paulista", retornoEsperado: "AVENIDA"},
+		{enderecoInserido: "Estrada do Sol", retornoEsperado: "ESTRADA"},
+		{enderecoInserido: "Rodovia BR470", retornoEsperado: "RODOVIA"},
+		{enderecoInserido: "Praça das Rosas", retornoEsperado: "INVALID TYPE"},
+		{enderecoInserido: "", retornoEsperado: "INVALID TYPE"},
 	}
 
+	for _, cenario := range cenariosDeTeste {
+		resultado := TipoDeEndereco(cenario.enderecoInserido)
+
+		if resultado != cenario.retornoEsperado {
+			test.Errorf("O tipo recebido foi %s e o esperado é %s, para o valor %s",
+				resultado,
+				cenario.retornoEsperado,
+				cenario.enderecoInserido,
+			)
+		}
+	}
 }
