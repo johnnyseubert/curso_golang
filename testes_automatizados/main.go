04fa6744a8ebac5a4507741ef1c133f3366f728a
JohnnySeubert <jbseubert@gmail.com>
2025-09-21 16:41:26 -0300

    Add test coverage report generation instructions and coverage data file

--- testes_automatizados/main.go
+++ testes_automatizados/main.go
@@ -5,6 +5,16 @@ import (
 	"testes/enderecos"
 )
 
+// go test --cover
+// go test ./...
+// go tool cover --html=cobertura.txt
+
+/*
+	Caso queira gerar um arquivo de cobertura de testes e mostrando o que nao esta coberto
+	go test --coverprofile cobertura.txt
+	go tool cover --html=cobertura.txt
+*/
+
 func main() {
 	result := enderecos.TipoDeEndereco("Avenida Paulista")
 
